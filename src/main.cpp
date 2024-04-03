cddaa259022f25e59ef509f2349db42a5298b310
Sparrowhawk120 <theworldofbirdsmaker@gmail.com>
2024-04-03 14:58:42 -0400

    change frequency, add timestamp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 struct packet {
-  float tankPrs, combnPrs, force;
+  float tankPrs, combnPrs, force, timeStamp;
 } dataPacket;
 
 char radiopacket[sizeof(dataPacket)];
@@ -19,6 +19,7 @@ float randFloat(float LO, float HI) {
 }
 
 uint32_t txCounter;
+uint32_t time;
 
 void setup() {
   pinMode(23,OUTPUT); digitalWrite(23,HIGH);
@@ -35,13 +36,13 @@ void setup() {
   if (!rf95.init())
     Serial.println("init failed");  
   else {Serial.println("init success");}
-  rf95.setFrequency(915.0); //set frequency to 915MHz
+  rf95.setFrequency(902.5); //set frequency to 915MHz
   rf95.setTxPower(20,false); //set the transmit power to 20dBm using PA_BOOST
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // You can change the modulation parameters with eg
   rf95.setModemConfig(RH_RF95::Bw125Cr45Sf128);
-  txCounter = millis();
+  txCounter = time = millis();
 }
 
 
@@ -50,18 +51,19 @@ void loop() {
   Serial.println(rf95.mode());
   int dT = millis()-txCounter;
   Serial.print("               dT: "); Serial.println((float)dT/1000.f,3);
+  float timeElapsed = (float)(millis()-time)/1000.f;
 
   if(rf95.mode() != rf95.RHModeTx) {
-    dataPacket = (packet){randFloat(750,850),randFloat(300,500),randFloat(1,800)};
+    dataPacket = (packet){randFloat(750,850),randFloat(300,500),randFloat(1,800),timeElapsed};
     memcpy(radiopacket,&dataPacket,sizeof(dataPacket));
     rf95.send((uint8_t *)radiopacket, sizeof(radiopacket));
     txCounter = millis();
     Serial.print("Sent: "); Serial.print(dataPacket.tankPrs,5); Serial.print(" "); Serial.print(dataPacket.combnPrs,5); Serial.print(" "); Serial.println(dataPacket.force,5);
   }
   
-  else if(dT > 200){
+  else if(dT > 50){
     rf95.setModeIdle();
-    Serial.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+    txCounter = millis();
   }
   Serial.print("Mode: ");
   Serial.println(rf95.mode()); Serial.println();
